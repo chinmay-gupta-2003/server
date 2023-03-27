bf7e124145345bedc98a0860724e071b203830a5
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-03-27 17:22:46 +0530

    Update add Post route

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,23 +1,21 @@
 const postModel = require("../models/postModel.js");
 
- const addPost = async (req,res,next)=>{
-    console.log('Hi');
-    console.log(req.file);
+exports.addPost = async (req,res)=>{
     const newPost = new postModel(req.body);
-    console.log(newPost);
     newPost.image = req.file.path;
-    console.log(newPost);
-    if(!req.body.userId || !req.body.date){
-        res.status(400).json({message:"Please fill all the fields"});
-    }
     try{
         const savePost = await newPost.save();
-        console.log(savePost);
-        res.status(200).json(savePost);
+        res.status(201).json({
+            status: 'success',
+            data: {
+              post: savePost,
+            },
+          });
     }
-    catch(err){
-        next(err);
-    }
+    catch (err) {
+        res.status(400).json({
+          status: 'fail',
+          message: err.message,
+        });
+      }
 }
-
-module.exports = {addPost};
